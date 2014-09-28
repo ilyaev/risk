6667019be18be41beb1b4af4c644b14ba3836bb6
yura.ilyaev <yura.ilyaev@YURA-T440P>
2014-09-28 11:34:22 -0700

    initial commit

---
+++ Risk-desktop/src/pbartz/games/risk/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pbartz.games.risk.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pbartz.games.risk.RiskGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RiskGame(), config);
+	}
+}
